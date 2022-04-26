2b9c547bafcb980e9b3abc99f1235397649479c1
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-04-26 17:13:41 +0900

    aaa

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float power = 10;
-    public Rigidbody rigidbody;
+    public Rigidbody rigibody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -19,11 +18,19 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidbody.AddForce(new Vector3(0, 0, 1) * power);
+            rigibody.AddForce(new Vector3(0, 0, 1) * power);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
+            rigibody.AddForce(new Vector3(-1,0,0)* power);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigibody.AddForce(new Vector3(0, 0, -1) * power);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigibody.AddForce(new Vector3(1, 0, 0) * power);
         }
     }
 }
